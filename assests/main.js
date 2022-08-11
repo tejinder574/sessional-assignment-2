0e20c3c587aeef1e97fb1595beb2c201864fc9ad
tejinder574 <tejinder0731.be20@chitkara.edu.in>
2022-08-11 18:30:11 +0530

    project done

--- assests/main.js
+++ assests/main.js
@@ -1,33 +1,52 @@
-document.querySelector('#search').addEventListener('click', () => {
-    const xhr = new XMLHttpRequest();
+// document.getElementById('submit').addEventListener('click', (e)=>{
+//     e.preventDefault();
+//     // const a = document.getElementById('file').value;
+//     // console.log(a);
+//     alert("Photo has been Submited");
+// })
 
-    const searchValue = document.querySelector('#my-text').value
 
-    const url = `https://jsonplaceholder.typicode.com/photos`
 
-    xhr.open('GET', url)
-
-    xhr.onreadystatechange = () => {
-        if(xhr.status === 200 && xhr.readyState === 4) {
-            const response = JSON.parse(xhr.responseText)
-            console.log(response)
-
-            let template = ''
-            for(let i=0; i < response.items.length; i++) {
-                template += `
-                    <div class="video-items" style="width: 20%; margin: 1.2em;">
-                        <a href="https://www.youtube.com/watch?v=${response.items[i].id.videoId}" target="_blank">
-                            <img style="width: 100%" src="${response.items[i].snippet.thumbnails.high.url}" />
-                        </a>
-                        <h5>${response.items[i].snippet.title}</h5>
-                        <p style="color: gray">${response.items[i].snippet.description}</p>
-                    </div>
-                `
-            }
-
-            document.querySelector('#my-container').innerHTML = template
-        }
+const xhr = new XMLHttpRequest()
+xhr.open('GET', 'https://jsonplaceholder.typicode.com/photos', true);
+xhr.onload= function(){
+    const response = JSON.parse(xhr.responseText);
+    // console.log(response[0].url);
+    // console.log(response[0]);
+    // console.log(response[99]);
+    let template ="";
+    let p=0;
+    const images=(i)=>{
+    for(i=p; i<p+50; i++){
+        let id = response[i].thumbnailUrl;
+        let title = response[i].title;
+        template += `
+        <div style="text-align;">
+            <img src="${id}" style="margin-right: 50px; margin-left:50px;">
+            <div style="width:150px; margin-left:50px; text-align: center;">
+            <p style="overflow:auto">${title}</p>
+            </div>
+        </div>`
     }
+    document.querySelector("#my-container").innerHTML = template
+    }
+    images(p);
+  
+    // document.getElementsByClassName('file').addEventListener('click', ()=>{
+    //     alert("Photo has been Submited");
+    // })
 
-    xhr.send()
-})
+    document.getElementById('more').addEventListener('click', ()=>{
+        console.log(p);
+        p = p+50;
+        if(p<5000){
+            images(p);
+        }
+    })
+}
+
+xhr.send()
+document.getElementsByClassName('file').addEventListener('click', ()=>{
+        alert("Photo has been Submited");
+    })
+
